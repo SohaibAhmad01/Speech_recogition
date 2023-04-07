1c788acb8281e015e7da8329bd496b09ef44d56d
SohaibAhmad01 <sohaibshafique303@gmail.com>
2023-04-07 14:31:14 +0500

    image to text Tesseract OCR

---
+++ imageToText.py
@@ -0,0 +1,14 @@
+import pytesseract
+from PIL import Image
+
+# Set Tesseract path (Windows example)
+pytesseract.pytesseract.tesseract_cmd = r":\Program Files\Tesseract-OCR"
+
+# Open image using PIL
+image = Image.open('./12.png')
+
+# Extract text from image
+text = pytesseract.image_to_string(image)
+
+# Print the extracted text
+print(text)
