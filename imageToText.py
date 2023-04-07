9169a9bd97f15b0980cbd57d13faed96bbd4e4a7
SohaibAhmad01 <sohaibshafique303@gmail.com>
2023-04-07 14:39:44 +0500

    file path change

--- imageToText.py
+++ imageToText.py
@@ -2,10 +2,10 @@ import pytesseract
 from PIL import Image
 
 # Set Tesseract path (Windows example)
-pytesseract.pytesseract.tesseract_cmd = r":\Program Files\Tesseract-OCR"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR"
 
 # Open image using PIL
-image = Image.open('./12.png')
+image = Image.open('12.png')
 
 # Extract text from image
 text = pytesseract.image_to_string(image)
